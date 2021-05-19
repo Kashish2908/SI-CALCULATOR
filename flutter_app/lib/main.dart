55a2ac91f5d187a4abb94916bb5bb31fc91e3d3a
Kashish Neekhra <kneekhra29@gmail.com>
2021-05-19 20:23:14 +0530

    SI CALCULATOR completed

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+import 'rowtutorial.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title:"Calculator",
+    home: Rowtutorial(),
+    debugShowCheckedModeBanner: false,
+  )
+  );
+}
+
